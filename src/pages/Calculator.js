b2ac947ba0665710e86a5e744b2c36d0edede89d
Elian <douglas.feliphe@going2.com.br>
2023-09-06 23:51:07 -0300

    feat: operation calculator (not finished yet)

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBoxOpen,
@@ -39,97 +39,166 @@ import { GeneralInfoForm } from '../components/Forms';
 
 import Profile3 from '../assets/img/team/profile-picture-3.jpg';
 
-const TABLE_DATA = [
-  {
-    operation: 'Operação #1',
-    odd: 0,
-    probability: 0,
-    valueToOperate: 0,
-  },
-  {
-    operation: 'Operação #2',
-    odd: 0,
-    probability: 0,
-    valueToOperate: 0,
-  },
-  {
-    operation: 'Operação #3',
-    odd: 0,
-    probability: 0,
-    valueToOperate: 0,
-  },
-];
-
 export default (props) => {
   // const { icon, iconColor, category, title, period, percentage } = props;
   // const percentageIcon = percentage < 0 ? faAngleDown : faAngleUp;
   // const percentageColor = percentage < 0 ? 'text-danger' : 'text-success';
+
+  const [totalInvested, setTotalInvested] = useState('');
+  const [value01, setValue01] = useState('');
+  const [value02, setValue02] = useState('');
+  const [value03, setValue03] = useState('');
+
+  const [Operation01, setOperation01] = useState(0);
+  const [Operation02, setOperation02] = useState(0);
+  const [Operation03, setOperation03] = useState(0);
+
+  const [invested01, setInvested01] = useState(0);
+  const [invested02, setInvested02] = useState(0);
+  const [invested03, setInvested03] = useState(0);
+
+  const [profit, setProfit] = useState(0);
+
+  function onChangeOdd(e, id) {
+    if (id === 0) setValue01(e.target.value);
+    if (id === 1) setValue02(e.target.value);
+    if (id === 2) setValue03(e.target.value);
+  }
+
+  function onChangeTotalInvested(e) {
+    setTotalInvested(e.target.value);
+  }
+
+  const handleCalculate = () => {
+    const sum = value03
+      ? 1 / parseFloat(value01) +
+        1 / parseFloat(value02) +
+        1 / parseFloat(value03)
+      : 1 / parseFloat(value01) + 1 / parseFloat(value02);
+
+    const Op01 = (1 / parseFloat(value01) / sum) * 100;
+    const Op02 = (1 / parseFloat(value02) / sum) * 100;
+    const Op03 = value03 ? (1 / parseFloat(value03) / sum) * 100 : 0;
+
+    setOperation01(Op01);
+    setOperation02(Op02);
+    setOperation03(Op03);
+
+    const inv01 = (parseFloat(totalInvested) * Operation01) / 100;
+    const inv02 = (parseFloat(totalInvested) * Operation02) / 100;
+    const inv03 = value03 ? (parseFloat(totalInvested) * Operation03) / 100 : 0;
+
+    setInvested01(inv01);
+    setInvested02(inv02);
+    setInvested03(inv03);
+
+    const prof = inv01 * parseFloat(value01);
+    setProfit(prof);
+
+    // Update state with the calculated values
+    setTotalInvested('');
+    setValue01('');
+    setValue02('');
+    setValue03('');
+
+    console.log(
+      Operation01.toFixed(2),
+      Operation02.toFixed(2),
+      Operation03.toFixed(2)
+    );
+  };
+
+  const TABLE_DATA = [
+    {
+      id: 0,
+      operation: 'Operação #1',
+      odd: value01,
+      probability: Operation01,
+      valueToOperate: invested01,
+    },
+    {
+      id: 1,
+      operation: 'Operação #2',
+      odd: value02,
+      probability: Operation02,
+      valueToOperate: invested02,
+    },
+    {
+      id: 2,
+      operation: 'Operação #3',
+      odd: value03,
+      probability: Operation03,
+      valueToOperate: invested03,
+    },
+  ];
+
   const Content = ({ value, subtitle }) => {
     return (
-      <>
-        <Card border='light' className='shadow-sm'>
-          <Card.Body>
-            <div className='d-flex justify-content-between'>
-              <div className=''>
-                <h5>{value}</h5>
-                <h3 className='mb-1'>{subtitle}</h3>
-              </div>
-              <div className='d-flex justify-content-center align-items-center'>
-                <span className='icon icon-md bg-secondary rounded-circle p-3'>
-                  <FontAwesomeIcon icon={faHome} />
-                </span>
-              </div>
+      <Card border="light" className="shadow-sm">
+        <Card.Body>
+          <div className="d-flex justify-content-between">
+            <div className="">
+              <h5>{value}</h5>
+              <h3 className="mb-1">{subtitle}</h3>
             </div>
-          </Card.Body>
-        </Card>
-      </>
+            <div className="d-flex justify-content-center align-items-center">
+              <span className="icon icon-md bg-secondary rounded-circle p-3">
+                <FontAwesomeIcon icon={faHome} />
+              </span>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
     );
   };
 
   return (
     <>
-      <div className='py-4'>
+      <div className="py-4">
         <Row>
           <Col>
-            <Content value={'BRL 00.0'} subtitle='Resultado' />
+            <Content value={'BRL 00.0'} subtitle="Resultado" />
           </Col>
           <Col>
-            <Content value={'00.0%'} subtitle='Lucro' />
+            <Content value={profit} subtitle="Lucro" />
           </Col>
         </Row>
-        <Row className='my-4'>
+        <Row className="my-4">
           <Col>
-            <Card border='light' className='shadow-sm'>
+            <Card border="light" className="shadow-sm">
               <Table>
-                <thead className='thead-light'>
+                <thead className="thead-light">
                   <tr>
-                    <th className='border-0'>OPERAÇÕES #</th>
-                    <th className='border-0'>ODDS </th>
-                    <th className='border-0'>% PROBABILIDADE</th>
-                    <th className='border-0'>VALOR A OPERAR</th>
+                    <th className="border-0">OPERAÇÕES #</th>
+                    <th className="border-0">ODDS </th>
+                    <th className="border-0">% PROBABILIDADE</th>
+                    <th className="border-0">VALOR A OPERAR</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {TABLE_DATA.map((data) => (
-                    <tr>
-                      <td className='border-1'>
+                  {TABLE_DATA.map((data, index) => (
+                    <tr key={index}>
+                      <td className="border-1">
                         <div>
-                          <span className='h6'>{data.operation}</span>
+                          <span className="h6">{data.operation}</span>
                         </div>
                       </td>
-                      <td className='border-0 fw-bold'>
+                      <td className="border-0 fw-bold">
                         <Form.Control
                           required
-                          type='number'
-                          placeholder='Ex: 80'
+                          type="number"
+                          placeholder="Ex: 80"
                           value={data.odd}
+                          onChange={(e) => onChangeOdd(e, data.id)}
                         />
                       </td>
-                      <td className='border-0 text-success'>
-                        <span className='fw-bold'>% {data.probability}</span>
+                      <td className="border-0 text-success">
+                        <span className="fw-bold">
+                          % {data.probability.toFixed(2)}
+                        </span>
                       </td>
-                      <td className='border-0 text-success'>
-                        <span className='fw-bold'>
+                      <td className="border-0 text-success">
+                        <span className="fw-bold">
                           R$ {data.valueToOperate}
                         </span>
                       </td>
@@ -140,14 +209,22 @@ export default (props) => {
             </Card>
           </Col>
         </Row>
-        <Row className='my-4'>
+        <Row className="my-4">
           <Col>
-            <span className='text-primary'>
+            <span className="text-primary">
               Informe o valor disponível para a operação:
             </span>
-            <Form.Control required type='number' placeholder='Ex: 80' />
-            <div className='my-2'>
-              <Button variant='secondary'>Calcular</Button>
+            <Form.Control
+              required
+              type="number"
+              placeholder="Ex: 80"
+              value={totalInvested}
+              onChange={onChangeTotalInvested}
+            />
+            <div className="my-2">
+              <Button variant="secondary" onClick={handleCalculate}>
+                Calcular
+              </Button>
             </div>
           </Col>
         </Row>
--- src/pages/TipsPremium.js
+++ src/pages/TipsPremium.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBoxOpen,
@@ -14,6 +14,8 @@ import {
   faStar,
   faFire,
   faSearch,
+  faCalculator,
+  faPlusCircle,
 } from '@fortawesome/free-solid-svg-icons';
 import {
   Col,
@@ -22,7 +24,11 @@ import {
   Dropdown,
   Card,
   Form,
+  Modal,
   InputGroup,
+  Badge,
+  Stack,
+  Collapse,
 } from '@themesberg/react-bootstrap';
 import { ChoosePhotoWidget, ProfileCardWidget } from '../components/Widgets';
 import { GeneralInfoForm } from '../components/Forms';
@@ -30,39 +36,114 @@ import { GeneralInfoForm } from '../components/Forms';
 import LogoPNG from '../assets/img/logo.png';
 
 export default () => {
+  const [showDefault, setShowDefault] = useState(false);
+  const handleClose = () => setShowDefault(false);
+
   return (
-    <div className='py-4'>
-      {/* <InputGroup>
-        <InputGroup.Text>
-          <FontAwesomeIcon icon={faSearch} />
-        </InputGroup.Text>
-        <Form.Control type='text' placeholder='Search' />
-      </InputGroup> */}
-      {/* <Form.Control /> */}
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant='top' src={LogoPNG} />
-        <Card.Body>
-          <div className='d-flex align-items-center'>
-            <div>
-              <Card.Text>5.0</Card.Text>
+    <>
+      <Modal as={Modal.Dialog} centered show={showDefault} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title className="h6 text-center">
+            CALCULADORA ANTIRED
+          </Modal.Title>
+          <Button variant="close" aria-label="Close" onClick={handleClose} />
+        </Modal.Header>
+        <Modal.Header className="d-flex flex-column justify-content-center">
+          <Modal.Title className="h6 text-center">
+            GEORGIA - ESPANHA
+          </Modal.Title>
+          Data do evento: 08/09/2023 ÀS 13:00 HS
+        </Modal.Header>
+        <Modal.Body>
+          <Modal.Title className="h6 text-center">
+            QUANTO VOCE QUER APOSTAR
+          </Modal.Title>
+          <Row>
+            <Col>
+              <div className="d-flex align-items-center">
+                <label>INVESTIR</label>
+                <Form.Control type="number" />
+              </div>
+            </Col>
+            =
+            <Col>
+              <div className="d-flex align-items-center">
+                <label>RETORNA</label>
+                <Form.Control className="bg-success text-secondary" />
+              </div>
+            </Col>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            I Got It
+          </Button>
+          <Button
+            variant="link"
+            className="text-gray ms-auto"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <h6>Operação</h6>
+      <Card>
+        <div className="d-flex justify-content-between p-3 align-items-center bg-light">
+          <div className="d-flex justify-content-center align-items-center">
+            <Button variant="text">
+              <FontAwesomeIcon icon={faPlusCircle} />
+            </Button>
+
+            <h5>FUTEBOL – VENEZUELA – SEGUNDA DIVISÃO</h5>
+          </div>
+
+          <h5>06/09/2023 ÀS 17:00 HS</h5>
+        </div>
+        <Collapse in={true}>
+          <div id="example-collapse-text">
+            <div className="p-3 bg-light">
+              <Row>
+                <Col xs={10}>
+                  <div>
+                    <div>EOVEGAS</div>
+                    <div>BETWAY</div>
+                  </div>
+                  <br />
+                  <div>
+                    <div>CS MARITIMO DE LA GUAIRA</div>
+                    <div>ATLETICO LA CRUZ</div>
+                  </div>
+                  <br />
+                  <div>
+                    <div>TIME 1 VENCE E EMPATE ANULA A APOSTA</div>
+                    <div>TIME 2 VENCE E EMPATE ANULA A APOSTA</div>
+                  </div>
+                </Col>
+                <Col>
+                  Odds 01:
+                  <Form.Control required type="number" placeholder="Ex: 80" />
+                  Odds 02:
+                  <Form.Control required type="number" placeholder="Ex: 80" />
+                </Col>
+              </Row>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faStar} className='me-2 text-warning' />
-            </div>
-            <div>
-              <Card.Text>17°</Card.Text>
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faFire} className='me-2 text-danger' />
+            <div className="d-flex justify-content-between align-items-center bg-white m-1">
+              <Button variant="success" disabled className="m-1">
+                <i>5,12% LUCRO GARANTIDO</i>
+              </Button>
+              <Button
+                variant="secondary"
+                className="m-1"
+                onClick={() => setShowDefault(true)}
+              >
+                <FontAwesomeIcon icon={faCalculator} className="me-2" />{' '}
+                Calcular
+              </Button>
             </div>
           </div>
-          <Card.Title>ANTIRED - Plano Anual</Card.Title>
-          <Card.Text>
-            Descubra o produto que irá revolucionar sua vida no mercado de...
-          </Card.Text>
-          <Button variant='secondary'>Ver Produto</Button>
-        </Card.Body>
+        </Collapse>
       </Card>
-    </div>
+    </>
   );
 };
